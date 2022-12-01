496ac0f9135f805a4d4dc99c0b59d7e9fb3a7098
ropi59 <mrseed@hotmail.com>
2022-12-01 09:59:11 +0100

    first commit

---
+++ src/main/java/fr/ropi/spring/teamsmanager/TeamsmanagerApplication.java
@@ -0,0 +1,13 @@
+package fr.ropi.spring.teamsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamsmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamsmanagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/ropi/spring/teamsmanager/TeamsmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ropi.spring.teamsmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
